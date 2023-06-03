e39a1e52d8c7dbb327bf77f43908fa4ee9e1ed72
Santeri Horttanainen <santeri.horttanainen@gmail.com>
2023-06-03 17:03:20 +0300

    Custom mesh for more vertices

--- bevy/hello/src/main.rs
+++ bevy/hello/src/main.rs
@@ -1,14 +1,12 @@
 use bevy::{
     prelude::*,
-    reflect::TypeUuid,
-    render::render_resource::{AsBindGroup, ShaderRef},
-    sprite::{MaterialMesh2dBundle, Material2d, Material2dPlugin}
+    sprite::{MaterialMesh2dBundle}, render::{render_resource::PrimitiveTopology, mesh::{Indices, VertexAttributeValues}}
 };
+use itertools::Itertools;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugin(Material2dPlugin::<CustomMaterial>::default())
         .add_startup_system(setup)
         .run();
 }
@@ -16,27 +14,116 @@ fn main() {
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<CustomMaterial>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
     commands.spawn(Camera2dBundle::default());
 
+     let mut land = Mesh::from(Land {
+        size: 2000.0,
+        num_vertices: 10,
+    });
+    if let Some(VertexAttributeValues::Float32x3(
+        positions,
+    )) = land.attribute(Mesh::ATTRIBUTE_POSITION)
+    {
+        let colors: Vec<[f32; 4]> = positions
+            .iter()
+            .map(|[r, g, b]| {
+                [
+                    (1. - *r) / 2.,
+                    (1. - *g) / 2.,
+                    (1. - *b) / 2.,
+                    1.,
+                ]
+            })
+            .collect();
+        land.insert_attribute(
+            Mesh::ATTRIBUTE_COLOR,
+            colors,
+        );
+    }
+
     commands.spawn(MaterialMesh2dBundle {
-        mesh: meshes.add(shape::Quad::new(Vec2 { x: 800.0, y: 500.0 }).into()).into(),
-        material: materials.add(CustomMaterial {
-        }),
-        transform: Transform::from_translation(Vec3::new(-150., 0., 0.)),
+        mesh: meshes.add(land).into(),
+        material: materials.add(ColorMaterial::default()),
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
         ..default()
     });
 
 }
 
-impl Material2d for CustomMaterial {
-    fn fragment_shader() -> ShaderRef {
-        "shaders/custom_material.wgsl".into()
+#[derive(Debug, Copy, Clone)]
+struct Land {
+    size: f32,
+    num_vertices: u32,
+}
+
+impl From<Land> for Mesh {
+    fn from(plane: Land) -> Self {
+        let extent = plane.size / 2.0;
+
+        let jump = extent / plane.num_vertices as f32;
+
+        let vertices = (0..=plane.num_vertices)
+            .cartesian_product(0..=plane.num_vertices)
+            .map(|(y, x)| {
+                [
+                    x as f32 * jump - 0.5 * extent,
+                    y as f32 * jump - 0.5 * extent,
+                    0.0,
+                ]
+            })
+            .collect::<Vec<_>>();
+
+        let indices = Indices::U32(
+            (0..=plane.num_vertices)
+                .cartesian_product(0..=plane.num_vertices)
+                .enumerate()
+                .filter_map(|(index, (x, y))| {
+                    if y >= plane.num_vertices {
+                        None
+                    } else if x >= plane.num_vertices {
+                        None
+                    } else {
+                        Some([
+                            [
+                                index as u32,
+                                index as u32
+                                    + 1
+                                    + 1
+                                    + plane.num_vertices,
+                                index as u32 + 1,
+                            ],
+                            [
+                                index as u32,
+                                index as u32
+                                    + 1
+                                    + plane.num_vertices,
+                                index as u32
+                                    + plane.num_vertices
+                                    + 1
+                                    + 1,
+
+                            ],
+                        ])
+                    }
+                })
+                .flatten()
+                .flatten()
+                .collect::<Vec<_>>(),
+        );
+
+        let positions: Vec<_> =
+            vertices.iter().map(|p| *p).collect();
+
+        let mut mesh =
+            Mesh::new(PrimitiveTopology::LineStrip);
+        mesh.set_indices(Some(indices));
+        mesh.insert_attribute(
+            Mesh::ATTRIBUTE_POSITION,
+            positions,
+        );
+        mesh
     }
 }
 
-#[derive(AsBindGroup, TypeUuid, Debug, Clone)]
-#[uuid = "f690fdae-d598-45ab-8225-97e2a3f056e0"]
-pub struct CustomMaterial {
-}
