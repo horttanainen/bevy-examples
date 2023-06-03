a229c76adfdfc95d28eff70b9b7bf4d0f3d19198
Santeri Horttanainen <santeri.horttanainen@gmail.com>
2023-06-03 12:26:35 +0300

    Custom material for a quad

--- bevy/hello/src/main.rs
+++ bevy/hello/src/main.rs
@@ -1,9 +1,45 @@
-use bevy::prelude::*;
+use bevy::{
+    prelude::*,
+    reflect::TypeUuid,
+    render::render_resource::{AsBindGroup, ShaderRef},
+    sprite::{MaterialMesh2dBundle, Material2d, Material2dPlugin}
+};
 
 fn main() {
-    App::new().add_system(hello_world_system).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(Material2dPlugin::<CustomMaterial>::default())
+        .add_startup_system(setup)
+        .run();
 }
 
-fn hello_world_system() {
-    println!("hello world");
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<CustomMaterial>>,
+) {
+    commands.spawn(Camera2dBundle::default());
+
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: meshes.add(shape::Quad::new(Vec2 { x: 800.0, y: 500.0 }).into()).into(),
+        material: materials.add(CustomMaterial {
+            color: Color::BLUE,
+        }),
+        transform: Transform::from_translation(Vec3::new(-150., 0., 0.)),
+        ..default()
+    });
+
+}
+
+impl Material2d for CustomMaterial {
+    fn fragment_shader() -> ShaderRef {
+        "shaders/custom_material.wgsl".into()
+    }
+}
+
+#[derive(AsBindGroup, TypeUuid, Debug, Clone)]
+#[uuid = "f690fdae-d598-45ab-8225-97e2a3f056e0"]
+pub struct CustomMaterial {
+    #[uniform(0)]
+    color: Color,
 }
