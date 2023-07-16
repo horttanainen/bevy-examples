5cfb32c7d6f594f3b0736f789d0909a387205243
Santeri Horttanainen <santeri.horttanainen@gmail.com>
2023-07-16 18:56:52 +0300

    Make a table where color changes if ball blocks visibility

--- bevy/hello/src/main.rs
+++ bevy/hello/src/main.rs
@@ -1,14 +1,101 @@
-use bevy::{
-    prelude::*,
-    sprite::{MaterialMesh2dBundle}, render::{render_resource::PrimitiveTopology, mesh::{Indices, VertexAttributeValues}}
-};
-use itertools::Itertools;
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+use rand::random;
+
+pub const TABLE_SIZE: IVec2 = IVec2::new(100, 100);
+pub const TILE_SIZE: Vec2 = Vec2::new(10., 10.);
+pub const BALL_RADIUS: f32 = 10.;
+
+#[derive(Component)]
+pub struct Tile;
+
+#[derive(Component)]
+pub struct CueBall;
+
+#[derive(Component)]
+pub struct Ball;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
+        .add_systems(Update, (draw_gizmos, color_tiles, move_cue_ball))
         .run();
+}
+
+fn color_tiles(
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    tiles: Query<(&mut Handle<ColorMaterial>, &Transform), With<Tile>>,
+    que_ball: Query<&Transform, With<CueBall>>,
+    balls: Query<&Transform, With<Ball>>,
+) {
+    let cue_ball_t = que_ball.single();
+    let ball_positions = balls.iter().map(|t| t.translation).collect();
+    for (cm, tile_t) in &tiles {
+        let color_mat = materials.get_mut(&cm).unwrap();
+        if !can_see_cue_ball(tile_t.translation, cue_ball_t.translation, &ball_positions) {
+            color_mat.color = Color::rgb(random::<f32>(), random::<f32>(), random::<f32>());
+        } else if color_mat.color != Color::GREEN {
+            color_mat.color = Color::GREEN;
+        }
+    }
+}
+
+fn move_cue_ball(
+    mut cue_ball: Query<(&mut Transform, With<CueBall>)>,
+    input: Res<Input<KeyCode>>,
+    time: Res<Time>,
+) {
+    for (mut transform, _) in &mut cue_ball {
+        if input.pressed(KeyCode::W) {
+            transform.translation.y += 100.0 * time.delta_seconds();
+        }
+        if input.pressed(KeyCode::S) {
+            transform.translation.y -= 100.0 * time.delta_seconds();
+        }
+        if input.pressed(KeyCode::D) {
+            transform.translation.x += 100.0 * time.delta_seconds();
+        }
+        if input.pressed(KeyCode::A) {
+            transform.translation.x -= 100.0 * time.delta_seconds();
+        }
+    }
+}
+
+fn can_see_cue_ball(tile_pos: Vec3, cue_ball_pos: Vec3, ball_positions: &Vec<Vec3>) -> bool {
+    let tile_pos2 = Vec3::new(tile_pos.x, tile_pos.y, 0.);
+    let cue_ball_pos2 = Vec3::new(cue_ball_pos.x, cue_ball_pos.y, 0.);
+    for ball_pos in ball_positions {
+        let ball_pos2 = Vec3::new(ball_pos.x, ball_pos.y, 0.);
+
+        let ball_to_cue_ball = cue_ball_pos2 - ball_pos2;
+        let tile_to_ball = ball_pos2 - tile_pos2;
+        let tile_to_cue_ball = cue_ball_pos2 - tile_pos2;
+
+        if Vec3::length_squared(tile_to_cue_ball) < Vec3::length_squared(tile_to_ball)
+            || Vec3::length_squared(tile_to_cue_ball) < Vec3::length_squared(ball_to_cue_ball)
+        {
+            continue;
+        }
+        let towards_cue_ball_until_ball = Vec3::dot(tile_to_ball, tile_to_cue_ball)
+            / Vec3::length_squared(tile_to_cue_ball)
+            * tile_to_cue_ball;
+        let distance = Vec3::length(tile_to_ball - towards_cue_ball_until_ball);
+        if distance <= BALL_RADIUS / 2. {
+            return false;
+        }
+    }
+    true
+}
+
+fn draw_gizmos(mut gizmos: Gizmos, tiles: Query<&Transform, With<Tile>>) {
+    for t in &tiles {
+        gizmos.rect_2d(
+            Vec2::new(t.translation.x, t.translation.y),
+            0.,
+            Vec2::new(TILE_SIZE.x, TILE_SIZE.y),
+            Color::RED,
+        );
+    }
 }
 
 fn setup(
@@ -18,112 +105,45 @@ fn setup(
 ) {
     commands.spawn(Camera2dBundle::default());
 
-     let mut land = Mesh::from(Land {
-        size: 2000.0,
-        num_vertices: 100,
-    });
-    if let Some(VertexAttributeValues::Float32x3(
-        positions,
-    )) = land.attribute(Mesh::ATTRIBUTE_POSITION)
-    {
-        let colors: Vec<[f32; 4]> = positions
-            .iter()
-            .map(|[r, g, b]| {
-                [
-                    *r / 500.,
-                    *g / 500.,
-                    *b / 500.,
-                    1.,
-                ]
-            })
-            .collect();
-        land.insert_attribute(
-            Mesh::ATTRIBUTE_COLOR,
-            colors,
-        );
+    for x in 0..TABLE_SIZE.x {
+        for y in 0..TABLE_SIZE.y {
+            let mut pos = IVec2::new(x - TABLE_SIZE.x / 2, y - TABLE_SIZE.y / 2);
+            pos *= IVec2::new(TILE_SIZE.x as i32, TILE_SIZE.y as i32);
+            commands.spawn((
+                MaterialMesh2dBundle {
+                    mesh: meshes
+                        .add(shape::Quad::new(Vec2::new(TILE_SIZE.x, TILE_SIZE.y)).into())
+                        .into(),
+                    material: materials.add(ColorMaterial::from(Color::GREEN)),
+                    transform: Transform::from_translation(Vec3::new(
+                        pos.x as f32,
+                        pos.y as f32,
+                        0.,
+                    )),
+                    ..default()
+                },
+                Tile,
+            ));
+        }
     }
 
-    commands.spawn(MaterialMesh2dBundle {
-        mesh: meshes.add(land).into(),
-        material: materials.add(ColorMaterial::default()),
-        transform: Transform::from_xyz(0.0, 0.5, 0.0),
-        ..default()
-    });
+    commands.spawn((
+        MaterialMesh2dBundle {
+            mesh: meshes.add(shape::Circle::new(BALL_RADIUS).into()).into(),
+            material: materials.add(ColorMaterial::from(Color::WHITE)),
+            transform: Transform::from_translation(Vec3::ONE),
+            ..default()
+        },
+        CueBall,
+    ));
 
+    commands.spawn((
+        MaterialMesh2dBundle {
+            mesh: meshes.add(shape::Circle::new(BALL_RADIUS).into()).into(),
+            material: materials.add(ColorMaterial::from(Color::BLUE)),
+            transform: Transform::from_translation(Vec3::ONE * 50.),
+            ..default()
+        },
+        Ball,
+    ));
 }
-
-#[derive(Debug, Copy, Clone)]
-struct Land {
-    size: f32,
-    num_vertices: u32,
-}
-
-impl From<Land> for Mesh {
-    fn from(plane: Land) -> Self {
-        let extent = plane.size / 2.0;
-
-        let jump = extent / plane.num_vertices as f32;
-
-        let vertices = (0..=plane.num_vertices)
-            .cartesian_product(0..=plane.num_vertices)
-            .map(|(y, x)| {
-                [
-                    x as f32 * jump - 0.5 * extent,
-                    y as f32 * jump - 0.5 * extent,
-                    0.0,
-                ]
-            })
-            .collect::<Vec<_>>();
-
-        let indices = Indices::U32(
-            (0..=plane.num_vertices)
-                .cartesian_product(0..=plane.num_vertices)
-                .enumerate()
-                .filter_map(|(index, (x, y))| {
-                    if y >= plane.num_vertices {
-                        None
-                    } else if x >= plane.num_vertices {
-                        None
-                    } else {
-                        Some([
-                            [
-                                index as u32,
-                                index as u32
-                                    + 1
-                                    + 1
-                                    + plane.num_vertices,
-                                index as u32 + 1,
-                            ],
-                            [
-                                index as u32,
-                                index as u32
-                                    + 1
-                                    + plane.num_vertices,
-                                index as u32
-                                    + plane.num_vertices
-                                    + 1
-                                    + 1,
-
-                            ],
-                        ])
-                    }
-                })
-                .flatten()
-                .flatten()
-                .collect::<Vec<_>>(),
-        );
-
-        let positions: Vec<_> =
-            vertices.iter().map(|p| *p).collect();
-
-        let mut mesh =
-            Mesh::new(PrimitiveTopology::LineStrip);
-        mesh.set_indices(Some(indices));
-        mesh.insert_attribute(
-            Mesh::ATTRIBUTE_POSITION,
-            positions,
-        );
-        mesh
-    }
-}
-
